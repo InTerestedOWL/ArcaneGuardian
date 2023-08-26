e329a0c5797ca0486caa8ec5afc83d03c17ce3d2
Christian Dechant <christian.dechant@abstractvoid.de>
2023-08-26 09:25:53 +0200

    Added the Startmenu and the settings

--- Assets/Scripts/MainMenu/SettingsMenu.cs
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -1,12 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public bool isBack;
     public GameObject settingsMenu;
     public GameObject mainMenu;
+    public AudioMixer audioVolumeMixer;
+    public AudioMixer audioEffectsMixer;
+    public Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     
     void OnMouseUp() {
         if (isBack)
@@ -14,5 +45,26 @@ public class SettingsMenu : MonoBehaviour
             settingsMenu.SetActive(false);
             mainMenu.SetActive(true);
         }
-    } 
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioVolumeMixer.SetFloat("volume", volume);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        audioEffectsMixer.SetFloat("effects", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
