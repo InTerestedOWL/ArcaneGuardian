6862cce625d98cf649b16f34f5689f77940bb140
Christian Jumtow <c.jumtow@gmx.de>
2023-09-10 18:57:48 +0200

    Remove unlearned skills vom action bar

--- Assets/Scripts/Combat/Skillbook.cs
+++ Assets/Scripts/Combat/Skillbook.cs
@@ -1,6 +1,8 @@
 using AG.Skills;
 using UnityEngine;
 using System.Collections.Generic;
+using AG.UI.Draggable;
+using AG.Actions;
 
 public class Skillbook : MonoBehaviour {
     [SerializeField]
@@ -8,12 +10,16 @@ public class Skillbook : MonoBehaviour {
 
     [SerializeField]
     List<Skill> learnedSkills;
+
     private SkillbookUI sbUI;
+    private GameObject actionBarReference;
     void Start() {
         sbUI = skillbookCollectionUIReference.GetComponent<SkillbookUI>();
         foreach (Skill skill in learnedSkills) {
             sbUI.AddToSkillbook(skill);
         }
+
+        actionBarReference = GameObject.Find("Action Bar Window");
     }
 
     public void AddSkill(Skill skill) {
@@ -24,5 +30,14 @@ public class Skillbook : MonoBehaviour {
     public void RemoveSkill(Skill skill) {
         learnedSkills.Remove(skill);
         sbUI.RemoveFromSkillbook(skill);
+
+        //Remove skill from actionbar
+        ActionBarSlotUI[] actionBarSlotUIs = actionBarReference.GetComponentsInChildren<ActionBarSlotUI>();
+        foreach (ActionBarSlotUI actionBarSlotUI in actionBarSlotUIs) {
+            ActionItem actionItem = actionBarSlotUI.GetItem();
+            if (actionItem?.GetDisplayName() == skill.GetDisplayName()) {
+                actionBarSlotUI.RemoveItems(0); //Warum muss ich bei der Methode einen int angeben?
+            }
+        }
     }
 }
