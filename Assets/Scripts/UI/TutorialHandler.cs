b4ada9943231925da4ea3e48223ca360ba6970da
Sven Schickentanz <Sven.Schickentanz@web.de>
2023-09-12 16:31:17 +0200

    FileHandler started

--- Assets/Scripts/UI/TutorialHandler.cs
+++ Assets/Scripts/UI/TutorialHandler.cs
@@ -1,15 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using AG.Files;
+using System;
 
 public class TutorialHandler : MonoBehaviour {
-    // Start is called before the first frame update
+    private FileHandler fh;
+
     void Start() {
-        
+        fh = ScriptableObject.CreateInstance<FileHandler>();
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public void ResetTutorial() {
+        TutorialEntry te = new("Test", false);
+        fh.Save(FileHandler.FileType.Tutorial, te);
+    }
+}
+
+public class TutorialEntry {
+    String name;
+    bool completed;
+
+    public TutorialEntry(string name, bool completed) {
+        this.name = name;
+        this.completed = completed;
     }
 }
