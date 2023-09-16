5be3eaffaec8cf20fd438996d01fc0abb47dab40
Christian Dechant <christian.dechant@abstractvoid.de>
2023-09-16 09:22:08 +0200

    Add the height of the followtarget to the ground height, so the minimap icon does not disappear

--- Assets/Scripts/MiniMap/MiniMap.cs
+++ Assets/Scripts/MiniMap/MiniMap.cs
@@ -7,11 +7,13 @@ public class MiniMap : MonoBehaviour
     [SerializeField]
     GameObject FollowTarget;
 
+    [SerializeField] private int heightOfIcon = 50;
+
     // Update is called once per frame
     void Update()
     {   
         //Update Position of Camera without Rotating it
         //We want our MiniMap to always be north facing
-        this.transform.position = new Vector3(FollowTarget.transform.position.x, this.transform.position.y, FollowTarget.transform.position.z);
+        this.transform.position = new Vector3(FollowTarget.transform.position.x, heightOfIcon + FollowTarget.transform.position.y, FollowTarget.transform.position.z);
     }
 }
