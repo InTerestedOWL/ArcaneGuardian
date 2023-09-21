357d8d777db839c54bd3d6978c385e5b7ca591fa
Sven Schickentanz <Sven.Schickentanz@web.de>
2023-09-21 23:53:50 +0200

    Kommentare angepast

--- Assets/Scripts/UI/Draggable/ActionBarSlotUI.cs
+++ Assets/Scripts/UI/Draggable/ActionBarSlotUI.cs
@@ -54,12 +54,10 @@ namespace AG.UI.Draggable {
             }
         }
         private void SetHotkeyUIDisplay() {
-            
             containerName = this.gameObject.transform.parent.gameObject.transform.parent.transform.name;
             
             if (hotkeyReference != null) {
                 // Takes only the first assigned key.
-                // TODO: Allow multiple keys to be assigned / Check for other keys.
                 hotkey = hotkeyReference.action.GetBindingDisplayString();
                 TMP_Text text = GetComponentInChildren<TMP_Text>();
                 if (text != null) {
@@ -116,54 +114,3 @@ namespace AG.UI.Draggable {
         }
     }
 }
-
-/*
-namespace AG.UI.Draggable {
-    public class ActionBarSlotUI : MonoBehaviour, IDragContainer<Sprite> {
-        [SerializeField] 
-        SkillIcon icon = null;
-        [SerializeField] 
-        InputActionReference hotkeyReference = null;
-        string hotkey = "";
-
-        private void Awake() {
-            SetHotkeyUIDisplay();
-        }
-
-        private void SetHotkeyUIDisplay() {
-            if (hotkeyReference != null) {
-                // Takes only the first assigned key.
-                // TODO: Allow multiple keys to be assigned / Check for other keys.
-                hotkey = hotkeyReference.action.GetBindingDisplayString();
-                TMP_Text text = GetComponentInChildren<TMP_Text>();
-                if (text != null) {
-                    text.text = hotkey;
-                }
-            }
-        }
-
-        public int MaxAcceptable(Sprite item) {
-            if (GetItem() == null) {
-                return int.MaxValue;
-            }
-            return 0;
-        }
-
-        public void AddItems(Sprite item, int number) {
-            icon.SetItem(item);
-        }
-
-        public Sprite GetItem() {
-            return icon.GetItem();
-        }
-
-        public int GetNumber() {
-            return 1;
-        }
-
-        public void RemoveItems(int number) {
-            icon.SetItem(null);
-        }
-    }
-}
-*/
